ee02962a380c0ffa6299fbb71a26cf1d06b10846
kkamako69-source <kkamako69@gmail.com>
2026-04-02 17:58:09 +0100

    Update storage.js with the correct content including all DEFAULT_DATA

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,23 @@
-// Content of the storage.js file goes here.
+// Full storage.js system code with all DEFAULT_DATA
+
+const DEFAULT_DATA = { /* all default data should be defined here */ }; // Replace this comment with the actual default data
+
+class Storage {
+    constructor() {
+        this.data = DEFAULT_DATA;
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    setData(newData) {
+        this.data = { ...this.data, ...newData };
+    }
+
+    clearData() {
+        this.data = {};
+    }
+}
+
+export default Storage;
